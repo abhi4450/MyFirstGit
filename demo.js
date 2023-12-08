c47be0a1ed25ae41315d806edbe1ddecbb20281d
abhi4450 <abhishek.anshu1991>
2023-12-08 13:54:04 +0530

    added index.html & demo.js

---
+++ demo.js
@@ -0,0 +1,20 @@
+//Examine the document object
+
+// console.dir(document);
+// console.log(document.URL);
+// console.log(document.title);
+// console.log(document.doctype)
+// console.log(document.forms);
+// console.log(document.links)
+// console.log(document.all);
+
+// document.title="123"
+
+let headerTitle = document.getElementById("header-title");
+
+headerTitle.style.borderBottom = "2px solid black";
+
+let subHeader = document.getElementById("subheader");
+
+subHeader.style.fontWeight = "bold";
+subHeader.style.color = "green";
